205c7c65260ccf5b46abb46976afdb48827715a7
Oleksandr Cherevko <oc.oleks.cherevko@gmail.com>
2023-08-01 13:42:53 +0300

    task2 done

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -17,3 +17,56 @@
 
     Додати iнкапсуляцiю до полiв та методiв якщо треба.
 */
+
+
+using Task_2;
+
+Violin violin = new Violin();
+
+
+violin.Sound();
+Console.WriteLine("-----");
+violin.Show();
+Console.WriteLine("-----");
+violin.Desc();
+Console.WriteLine("-----");
+violin.History();
+Console.WriteLine("-----");
+
+
+Trombone trombone = new Trombone();
+
+
+trombone.Sound();
+Console.WriteLine("-----");
+trombone.Show();
+Console.WriteLine("-----");
+trombone.Desc();
+Console.WriteLine("-----");
+trombone.History();
+Console.WriteLine("-----");
+
+Ukulele ukulele = new Ukulele();
+
+ukulele.Sound();
+Console.WriteLine("-----");
+ukulele.Show();
+Console.WriteLine("-----");
+ukulele.Desc();
+Console.WriteLine("-----");
+ukulele.History();
+Console.WriteLine("-----");
+
+
+
+Cello cello = new Cello();
+
+cello.Sound();
+Console.WriteLine("-----");
+cello.Show();
+Console.WriteLine("-----");
+cello.Desc();
+Console.WriteLine("-----");
+cello.History();
+Console.WriteLine("-----");
+
